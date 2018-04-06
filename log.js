5250398d9014c28cbeefc30236152d84c4161480
weichaocai <ccsobj@163.com>
2018-04-06 15:01:04 +0800

    update pm2

--- log.js
+++ log.js
@@ -3,22 +3,29 @@
  */
 
 
-var log4js = require('log4js');
+const log4js = require('log4js');
 
+// NOTE: for PM2 support to work you'll need to install the pm2-intercom module
+// `pm2 install pm2-intercom`
 log4js.configure({
     appenders: {
         out: { type: 'stdout' },//设置是否在控制台打印日志
         info: { type: 'file', filename: './logs/info.log' }
     },
     categories: {
-        default: { appenders: [ 'out', 'info' ], level: 'info' }//去掉'out'。控制台不打印日志
-    }
+        default: { appenders: [ 'out', 'info' ], level: 'info' }
+    },
+    pm2: true,
+    pm2InstanceVar: 'INSTANCE_ID'
 });
 
 
 
 module.exports.logger = function(name){
     var logger = log4js.getLogger(name);
+    setTimeout(function(){
+        logger.info("I'm forever blowing bubbles");
+    }, 1000);
     return logger;
 }
 

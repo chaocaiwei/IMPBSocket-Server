a27bf68cbc65cadbf4f66d2a652fd451a4d812ff
weichaocai <ccsobj@163.com>
2018-04-08 20:50:33 +0800

    update

--- routes/Base.js
+++ routes/Base.js
@@ -4,8 +4,8 @@
 
 
 var base = require("../impb/base_pb");
-var User = base.UserInfo;
-var IP   = base.IpInfo;
+var User = base.User_info;
+var IP   = base.Ip_info;
 
 // 从数据库拿到的user  转化为PB的user结构
 module.exports.builderFromUser = function (user) {
